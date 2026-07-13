ab00004e0cb91f6649dfd67f1dc68cadfb8e3724
Sanika Chougule <chougulesanika50@gmail.com>
2026-07-13 10:43:15 +0530

    Single number

--- Arrays/singleNumber.java
+++ Arrays/singleNumber.java
@@ -1,18 +1,27 @@
 class singleNumber{
 	public static void main(String args[])
 	{
-		int nums[]={2,2,1};
+		int nums[]={4,2,1,2,1};
 	
 		int n=0;
        		for(int i=0;i<nums.length;i++){
             		int c=0;
-            		for(int j=i+1;j<nums.length-1;j++){
-                		if(nums[i]==nums[j]){
+            		for(int j=0;j<nums.length;j++){
+				System.out.println(nums[i]+" = "+nums[j]);
+                		if(nums[i]==nums[j]&&i!=j){
+					
                     			c++;
+					System.out.println(nums[i]+" = "+nums[j]+"->"+c);
                 		}
+				
             		}
+			if(c==0){
+                			n=nums[i];
+            			}
+			
 		}
-            	if(c==0){
-                	n=nums[i];
-            }
+            	
+		System.out.println(n);
         }
+	
+}
